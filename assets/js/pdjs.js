16f388744e87ea30f9bd04ffc1e234e2cfca2852
Jesse Hamilton <acostoss@gmail.com>
2014-09-28 17:06:28 -0400

    rewrote html as divs instead of tables

--- assets/js/pdjs.js
+++ assets/js/pdjs.js
@@ -19,7 +19,7 @@
       }
       this.subdomain = params.subdomain;
       this.token = params.token;
-      this.refresh = params.refresh || 60;
+      this.refresh = params.refresh || 3600;
       this.refresh_in_ms = this.refresh * 1000;
       this.protocol = params.protocol || "https";
       this.server = params.server || "pagerduty.com";
@@ -120,55 +120,6 @@
       };
       this.logg(params);
       return this.api(params);
-    };
-
-    PDJSobj.prototype.event = function(params) {
-      var _this = this;
-      if (params == null) {
-        params = {};
-      }
-      this.logg("Create an event");
-      params.type = "POST";
-      params.url = params.url || this.protocol + "://events." + this.server + "/generic/2010-04-15/create_event.json";
-      params.data = params.data || {};
-      params.data.service_key = params.data.service_key || params.service_key || this.logg("No service key");
-      params.data.event_type = params.data.event_type || params.event_type || "trigger";
-      params.data.description = params.data.description || params.description || "No description provided";
-      params.data.details = params.data.details || params.details || {};
-      params.data = JSON.stringify(params.data);
-      params.contentType = "application/json; charset=utf-8";
-      params.dataType = "json";
-      params.error = params.error || function(err) {
-        return _this.error_function(err, params);
-      };
-      params.success = params.success || function(data) {
-        return _this.no_success_function(data, params);
-      };
-      return $.ajax(params);
-    };
-
-    PDJSobj.prototype.trigger = function(params) {
-      if (params == null) {
-        params = {};
-      }
-      params.event_type = "trigger";
-      return this.event(params);
-    };
-
-    PDJSobj.prototype.acknowledge = function(params) {
-      if (params == null) {
-        params = {};
-      }
-      params.event_type = "acknowledge";
-      return this.event(params);
-    };
-
-    PDJSobj.prototype.resolve = function(params) {
-      if (params == null) {
-        params = {};
-      }
-      params.event_type = "resolve";
-      return this.event(params);
     };
 
     return PDJSobj;
